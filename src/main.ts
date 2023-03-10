3fd06fbfb4dff07697fc5a580ea7f2e00771edf8
NikitaSdev <nikitashadowrazev@gmail.com>
2023-03-10 19:35:13 +0300

    a

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,12 @@ import { NestFactory } from "@nestjs/core"
 import { AppModule } from "./app.module"
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin: "http://1269893-cf00631.tw1.ru",
+      credentials: true
+    }
+  })
   app.setGlobalPrefix("api")
   app.enableCors()
   await app.listen(5000)
